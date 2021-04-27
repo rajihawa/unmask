fb260342e2478020f94c8d1223c2ce6657817eb1
rajihawa <46829217+rajihawa@users.noreply.github.com>
2021-04-27 20:02:22 +0300

    created projecr type

---
+++ domain/project.go
@@ -0,0 +1,31 @@
+package domain
+
+import "time"
+
+// Project - the project type for the app
+type Project struct {
+	ID          string
+	Name        string
+	Description string
+	UserCount   int
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+// The ProjectRepo will have all the database functions for the project type
+type ProjectRepo interface {
+	GetOne(id string) (*Project, error)
+	GetAll(limit int, offset int) ([]Project, error)
+	UpdateOne(id string, newProject Project) error
+	CreateOne(newProject Project) error
+	DeleteOne(id string) error
+}
+
+// The ProjectUsecases will contain all the business logic of the projects
+type ProjectUsecases interface {
+	GetProject(id string) (*Project, error)
+	GetProjects(limit int, offset int) ([]Project, error)
+	UpdateProject(id string, newProject Project) error
+	CreateProject(newProject Project) error
+	DeleteProject(id string) error
+}
