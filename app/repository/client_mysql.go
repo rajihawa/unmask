1d5d167fbb64fd8042f3f797ee5e5b965df00f21
rajihawa <46829217+rajihawa@users.noreply.github.com>
2021-05-06 15:26:28 +0300

    added some client repo functions

--- app/repository/client_mysql.go
+++ app/repository/client_mysql.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"database/sql"
 
+	"github.com/Masterminds/squirrel"
 	"github.com/rajihawa/unmask/app/data"
 	"github.com/rajihawa/unmask/app/domain"
 )
@@ -18,15 +19,46 @@ func NewClientMySqlRepo() domain.ClientRepo {
 }
 
 func (c ClientMySqlRepo) GetOne(id string) (*domain.Client, error) {
-	panic("not implemented") // TODO: Implement
+	clientQuery := squirrel.Select("*").From("clients").Where(squirrel.Eq{"id": id})
+	rows, err := clientQuery.RunWith(c.db).Query()
+	if err != nil {
+		return nil, err
+	}
+	emptyClient := domain.Client{}
+	for rows.Next() {
+		err := rows.Scan(&emptyClient.ID, &emptyClient.Secret, &emptyClient.Name, &emptyClient.Description, &emptyClient.HomeURL, &emptyClient.CallbackURL, &emptyClient.Privileges, &emptyClient.AutoVerify, &emptyClient.CreatedAt, &emptyClient.UpdatedAt, &emptyClient.ProjectID)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &emptyClient, nil
 }
 
 func (c ClientMySqlRepo) GetAll(limit int, offset int) ([]domain.Client, error) {
-	panic("not implemented") // TODO: Implement
+	clientsQuery := squirrel.Select("*").From("clients").Limit(uint64(limit)).Offset(uint64(offset))
+	rows, err := clientsQuery.RunWith(c.db).Query()
+	if err != nil {
+		return nil, err
+	}
+	emptyClients := []domain.Client{}
+	for rows.Next() {
+		emptyClient := domain.Client{}
+		err := rows.Scan(&emptyClient.ID, &emptyClient.Secret, &emptyClient.Name, &emptyClient.Description, &emptyClient.HomeURL, &emptyClient.CallbackURL, &emptyClient.Privileges, &emptyClient.AutoVerify, &emptyClient.CreatedAt, &emptyClient.UpdatedAt, &emptyClient.ProjectID)
+		if err != nil {
+			return nil, err
+		}
+		emptyClients = append(emptyClients, emptyClient)
+	}
+	return emptyClients, nil
 }
 
 func (c ClientMySqlRepo) UpdateOne(id string, newClient domain.Client) error {
-	panic("not implemented") // TODO: Implement
+	updateQuery := squirrel.Update("clients").Set("name", newClient.Name).Set("description", newClient.Description).Set("home_url", newClient.HomeURL).Set("callback_url", newClient.CallbackURL).Set("privileges", newClient.Privileges).Set("auto_verify", newClient.AutoVerify).Where(squirrel.Eq{"id": id})
+	_, err := updateQuery.RunWith(c.db).Exec()
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func (c ClientMySqlRepo) CreateOne(newClient domain.Client) error {
