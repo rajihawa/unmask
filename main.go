92eca9713c5181c846445c4166edbae54a238fa2
rajihawa <46829217+rajihawa@users.noreply.github.com>
2021-05-06 14:49:05 +0300

    setup main.go

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/rajihawa/unmask/app"
+	"github.com/rajihawa/unmask/app/domain"
+)
 
+var appConfig = app.AppConfig{DB: domain.DatabaseConfig{
+	Driver:   "mysql",
+	Host:     "localhost",
+	Database: "db",
+	Port:     "3306",
+	Username: "user",
+	Password: "password",
+},
+	Env: "development"}
+
+func main() {
+	app := app.InitApp(appConfig)
+	defer app.Close()
 }
