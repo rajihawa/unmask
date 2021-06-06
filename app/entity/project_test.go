28d25f9d6d4e310f7229916c2a41d7955f1ced9c
rajihawa <46829217+rajihawa@users.noreply.github.com>
2021-06-06 14:02:15 +0300

    add project tests

---
+++ app/entity/project_test.go
@@ -0,0 +1,18 @@
+package entity_test
+
+import (
+	"testing"
+
+	"github.com/rajihawa/mask-off/app/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+var name = "New project"
+var description = "a new project for testing."
+
+func TestNewProject(t *testing.T) {
+	p := entity.NewProject(name, description)
+	assert.Equal(t, p.Name, name)
+	assert.Equal(t, p.Description, description)
+	assert.NotNil(t, p.ID)
+}
